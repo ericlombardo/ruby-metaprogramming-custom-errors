ed6737a773aebf6663fa185ac808e722c21b41d7
Eric <eric.m.lombardo@gmail.com>
2020-12-10 11:22:15 -0500

    learning how to raise custom errors and rescue code with messages

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -6,8 +6,23 @@ class Person
   end
 
   def get_married(person)
-    self.partner = person
-    person.partner = self
+    self.partner = person           # assings instance.partner to person they married
+    if person.class != Person       # checks if input is an instance of a person
+      begin                         # starts error handling
+        raise PartnerError          # if not an instance PartnerError is thrown 
+      rescue PartnerError => error  # method is rescued and error is placeholder for error message
+        puts error.message          # puts error.message (aka PartnerError.message)
+      end
+    else
+      person.partner = self         # if person is an instance, assigns this instance to them as their partner
+    end
+
+  end
+  
+  class PartnerError < StandardError
+    def message
+      "you must give the get_married method an argument of an instance of the person class!"
+    end
   end
 
 end
